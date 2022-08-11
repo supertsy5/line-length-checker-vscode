71d55108395e156a39966551606128fc1c55300c
0x7f178735 <0x7f178735@gmail.com>
2022-08-12 02:57:37 +0800

    20220812

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,47 @@
 import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
-		vscode.workspace.onDidChangeTextDocument(event => {
-		let i = 0;
-			if (event.document !== undefined) {
-				let config = vscode.workspace.getConfiguration('line-length-highlighter');
-				let max = config.lineLength;
-				while (i < event.document.lineCount) {
-					if (event.document.lineAt(i).text.length >= max) {
-					console.log("A line was changed and caught by line-length-highlighter bitch");
-					let number = (i+1).toString();
-					vscode.window.showInformationMessage("Line " + number + " too long");
-				}
-				i++;
-			}
+	let collection = vscode.languages.createDiagnosticCollection('overlength');
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (event.document !== undefined) {
+			checkOverlength(collection, event.document);
 		}
+	});
+	vscode.workspace.onDidOpenTextDocument(document => {
+		checkOverlength(collection, document);
+	});
+	vscode.workspace.textDocuments.forEach(document => {
+		checkOverlength(collection, document);
 	});
 }
 export function deactivate() {}
+
+function checkOverlength(
+	set: vscode.DiagnosticCollection,
+	document: vscode.TextDocument
+) {
+	if(document.uri.path.endsWith('.git')){
+		return;
+	}
+	console.log("Checking: " + document.uri.toString());
+	let config = vscode.workspace.getConfiguration('line-length-highlighter');
+	let max = config.lineLength;
+	let i = 0;
+	let diags: vscode.Diagnostic[] = [];
+	while (i < document.lineCount) {
+		let length = document.lineAt(i).text.length;
+		if (length >= max) {
+			diags.push(
+				new vscode.Diagnostic(
+					new vscode.Range(
+						new vscode.Position(i, 0),
+						new vscode.Position(i, length)
+					),
+					`Overlength line: ${length} chars`,
+					vscode.DiagnosticSeverity.Warning
+				)
+			);
+		}
+		i++;
+	}
+	set.set(document.uri, diags);
+}
