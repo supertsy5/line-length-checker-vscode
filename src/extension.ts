9b5028845b938fa6347fdcc8a5237d5c66946a1f
0x7f178735 <0x7f178735@gmail.com>
2022-08-15 23:07:30 +0800

    Added workspaceOnly and made some tiny changes

--- src/extension.ts
+++ src/extension.ts
@@ -3,14 +3,17 @@ export function activate(context: vscode.ExtensionContext) {
 	let config = vscode.workspace.getConfiguration('line-length-checker');
 	let max = config.lineLength as number;
 	let blacklist = config.blacklist as string[];
-	let filters = (
+	let blacklistFilters = (
 		vscode.workspace.workspaceFolders?.flatMap(
-			wsf => blacklist.map(pattern => ({
-				pattern: new vscode.RelativePattern(wsf, pattern),
-				scheme: 'file'
+			folder => blacklist.map(pattern => ({
+				pattern: new vscode.RelativePattern(folder, pattern)
 			}))
 		) as vscode.DocumentFilter[] ?? []
 	).concat({scheme: 'git'}, {scheme: 'vscode'});
+	let workspaceFilters = vscode.workspace.workspaceFolders?.map(
+		folder => ({pattern: new vscode.RelativePattern(folder, '*')})
+	);
+	let workspaceOnly = config.workspaceOnly as boolean;
 
 	let collection = vscode.languages.createDiagnosticCollection('overlength');
 
@@ -29,7 +32,12 @@ export function activate(context: vscode.ExtensionContext) {
 	function checkOverlength(
 		document: vscode.TextDocument
 	) {
-		if(vscode.languages.match(filters, document)) {
+		if(
+			(
+				workspaceOnly && workspaceFilters
+				&& !vscode.languages.match(workspaceFilters, document)
+			) || vscode.languages.match(blacklistFilters, document)
+			) {
 			console.log("Skipped: " + document.uri.toString());
 			return;
 		}
